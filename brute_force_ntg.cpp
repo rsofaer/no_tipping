1a8725c116557ac1cd5d600f37b8bd5467482afd
Brandon Reiss <brandon.l.reiss@gmail.com>
2011-10-02 13:07:59 -0400

    * Incorporate DoPly()/UndoPly() optimization for tree traversal. Now, each   visit call need only keep track of its plys since the child visits will   reverse any changes to the state before returning from the call. TODO: * Place code into generic no tipping header. * Impl board evaluation function. * Impl MiniMax. * Impl Java<->Cpp infrastructure.

--- brute_force_ntg.cpp
+++ brute_force_ntg.cpp
@@ -11,13 +11,27 @@ typedef int Weight;
 
 // Default game parameters.
 enum { DefaultWeightCount = 7, };
+enum { BoardSize = 10, };
+enum { BoardPivotL = -3, };
+enum { BoardPivotR = -1, };
 enum { BoardWeight = 3, };
 enum { BoardCofG = 0, };
 
 /// <summary> A game board including played weights. </summary>
+/// <remarks>
+///   <para> The overloaded array operator is not zero-based. It is designed
+///     for the layout of the game board such that Board[0] is the board
+///     center, Board[n: n < 0] is the left side of the board and
+///     Board[n: n > 0] is the right side of the board. For a board of size
+///     N, the allowable indices are -N <= n <= N.
+///   </para>
+/// </remarks>
 template <int PivotL_, int PivotR_, int Size_>
 struct GenericBoard
 {
+  enum { Size = Size_ };
+  enum { PivotL = PivotL_ };
+  enum { PivotR = PivotR_ };
   enum { Empty = 0, };
 
   inline void SetPos(const int pos, const Weight w)
@@ -25,6 +39,8 @@ struct GenericBoard
     assert(pos >= -size);
     assert(pos <= size);
     // rsofaer -- 20110929 -- Assert board not tipped.
+    // reissb -- 20111002 -- This should go into an external function
+    //   bool BoardTipped(const Board&).
     positions[pos + 10] = w;
   }
 
@@ -43,9 +59,6 @@ struct GenericBoard
     return positions[pos + 10];
   }
 
-  enum { Size = Size_ };
-  enum { PivotL = PivotL_ };
-  enum { PivotR = PivotR_ };
   Weight positions[(2 * Size) + 1];
 };
 
@@ -59,7 +72,7 @@ struct GenericPlayer
   int remain;
 };
 
-typedef GenericBoard<-3, -1, 10> Board;
+typedef GenericBoard<BoardPivotL, BoardPivotR, BoardSize> Board;
 typedef GenericPlayer<DefaultWeightCount> Player;
 
 /// <summary> A game state. Consists of board and players. </summary>
@@ -72,10 +85,18 @@ struct State
     Turn_Count,
   };
 
+  enum Phase
+  {
+    Phase_Adding = 0,
+    Phase_Removing,
+    Phase_Count,
+  };
+
   Board board;
   Player red;
   Player blue;
   Turn turn;
+  Phase phase;
 };
 
 /// <summary> Get the current player from the state. </summary>
@@ -112,22 +133,27 @@ inline void InitBoard(Board* board)
 inline void InitState(State* state)
 {
   assert(state);
-  state->turn = State::Turn_Red;
+  InitBoard(&state->board);
   InitPlayer(&state->red);
   InitPlayer(&state->blue);
-  InitBoard(&state->board);
+  state->turn = State::Turn_Red;
+  state->phase = State::Phase_Adding;
 }
 
 /// <summary> Unexplored move that will spawn a new state. </summary>
+/// <remarks>
+///   <para> The ply must be managed in conjunction with its associated state.
+///     the state is not copied for performance reasons. Note that all plys
+///     are easily reversible. This property is ideal for tree traversal.
+///   </para>
+/// </remarks>
 struct Ply
 {
-  Ply(const State& state_, const int target_, const Weight weight_)
-    : state(state_),
-      target(target_),
+  Ply(const int target_, const Weight weight_)
+    : target(target_),
       weight(weight_)
   {}
 
-  State state;
   int target;
   Weight weight;
 };
@@ -148,6 +174,13 @@ int Torque(const Board& board)
 }
 
 /// <summary> Find all plys from a given state. </summary>
+/// <remarks>
+///   <para> The supplied LeftPivotOp determines the plys that are kept when
+///     testing against the left pivot. The right pivot uses the opposite
+///     logic. Passing the correct operator will yield non-suicidal moves,
+///     suicidal moves, or similar sets with different boundaries.
+///   </para>
+/// </remarks>
 template <typename LeftPivotOp>
 void ExpandState(const State& state, std::vector<Ply>* plys)
 {
@@ -184,7 +217,7 @@ void ExpandState(const State& state, std::vector<Ply>* plys)
         }
         else
         {
-          plys->push_back(Ply(state, pos, *w));
+          plys->push_back(Ply(pos, *w));
         }
       }
     }
@@ -195,7 +228,7 @@ void ExpandState(const State& state, std::vector<Ply>* plys)
       {
         continue;
       }
-      plys->push_back(Ply(state, pos, *w));
+      plys->push_back(Ply(pos, *w));
     }
     // Compute for right pivot.
     {
@@ -214,7 +247,7 @@ void ExpandState(const State& state, std::vector<Ply>* plys)
         }
         else
         {
-          plys->push_back(Ply(state, pos, *w));
+          plys->push_back(Ply(pos, *w));
         }
       }
     }
@@ -248,56 +281,70 @@ inline void SuicidalPlys(const State& state, std::vector<Ply>* plys)
 }
 
 /// <summary> Apply the ply and get the resulting state. </summary>
-inline void DoPly(const Ply& ply, State* result)
+/// <remarks>
+///   <para> This function will mutate the incoming state. Its actions may
+///     be undone easily with UndoPly(const Ply&, State*).
+///   </para>
+/// </remarks>
+inline void DoPly(const Ply& ply, State* state)
 {
-  assert(result);
-  assert(Board::Empty == ply.state.board[ply.target]);
-  *result = ply.state;
-  // Update the board.
-  result->board[ply.target] = ply.weight;
-  // Swap the active player.
+  assert(state);
+  if (State::Phase_Adding == state->phase)
   {
-    State::Turn& turn = result->turn;
-    ++reinterpret_cast<int&>(turn);
-    reinterpret_cast<int&>(turn) %= State::Turn_Count;
+    assert(Board::Empty == state->board[ply.target]);
+    // Update the board.
+    state->board[ply.target] = ply.weight;
+    // Swap the active player.
+    {
+      State::Turn& turn = state->turn;
+      ++reinterpret_cast<int&>(turn);
+      reinterpret_cast<int&>(turn) %= State::Turn_Count;
+    }
+  }
+  else
+  {
+    assert(State::Phase_Removing == state->phase);
+    assert(ply.weight == state->board[ply.target]);
+    // Update the board.
+    state->board[ply.target] = Board::Empty;
+    // Swap the active player.
+    {
+      State::Turn& turn = state->turn;
+      ++reinterpret_cast<int&>(turn);
+      reinterpret_cast<int&>(turn) %= State::Turn_Count;
+    }
   }
 }
 
-/// <summary> Apply the ply and get the resulting state. </summary>
-inline State DoPly(const Ply& ply)
+/// <summary> Revserse the ply and get the resulting state. </summary>
+inline void UndoPly(const Ply& ply, State* state)
 {
-  State newState;
-  DoPly(ply, &newState);
-  return newState;
-}
-
-/// <summary> Count possible plys in the game brute-force. </summary>
-long long CountPossiblePlys(const State& root)
-{
-  long long plys = 0LL;
-  std::vector<State> plysToExplore;
-  plysToExplore.push_back(root);
-  std::vector<Ply> currentPlys;
-  long long loopCount = 0;
-  enum { LoopsToReport = 1000000, };
-  do
+  assert(state);
+  if (State::Phase_Adding == state->phase)
   {
-    const State* currentState = &plysToExplore.back();
-    currentPlys.clear();
-    PossiblePlys(*currentState, &currentPlys);;
-    plysToExplore.pop_back();
-    std::transform(currentPlys.begin(), currentPlys.end(),
-                   std::back_inserter(plysToExplore),
-                   std::ptr_fun<const Ply&, State>(DoPly));
-    plys += currentPlys.size();
-    if (0 == (++loopCount % LoopsToReport))
+    assert(ply.weight == state->board[ply.target]);
+    // Update the board.
+    state->board[ply.target] = Board::Empty;
+    // Swap the active player.
     {
-      std::cout << "After " << loopCount << " loops found "
-                << plys << " plys with " << plysToExplore.size()
-                << " plys left to explore." << std::endl;
+      State::Turn& turn = state->turn;
+      ++reinterpret_cast<int&>(turn);
+      reinterpret_cast<int&>(turn) %= State::Turn_Count;
     }
-  } while (!plysToExplore.empty());
-  return plys;
+  }
+  else
+  {
+    assert(State::Phase_Removing == state->phase);
+    assert(Board::Empty == state->board[ply.target]);
+    // Update the board.
+    state->board[ply.target] = ply.weight;
+    // Swap the active player.
+    {
+      State::Turn& turn = state->turn;
+      ++reinterpret_cast<int&>(turn);
+      reinterpret_cast<int&>(turn) %= State::Turn_Count;
+    }
+  }
 }
 
 /// <summary> Functional helper to identify states that conflict a given
@@ -305,11 +352,15 @@ long long CountPossiblePlys(const State& root)
 /// </summary>
 struct ConflictWithBoard
 {
+  /// <summary> Fast lookup to comparison board. </summary>
   typedef std::map<int, int> BoardMap;
 
-  ConflictWithBoard(const Board& board)
-    : boardMap()
+  ConflictWithBoard(const Board& board, State* state_)
+    : boardMap(),
+      state(state_)
   {
+    assert(state);
+    // Compute fast lookup to board.
     for (int pos = -Board::Size; pos <= Board::Size; ++pos)
     {
       const Weight w = board[pos];
@@ -320,9 +371,16 @@ struct ConflictWithBoard
     }
   }
 
-  inline bool operator()(const State& state) const
+  /// <summary> Test the given ply at the base state for conflicts to
+  ///   the comparison board.
+  /// </summary>
+  inline bool operator()(const Ply& ply) const
   {
-    const Board& board = state.board;
+    // Mutate the board with this ply.
+    DoPly(ply, state);
+    // Find conflict in the board to the comparison board.
+    const Board& board = state->board;
+    bool conflict = false;
     for (BoardMap::const_iterator boardPos = boardMap.begin();
          boardPos != boardMap.end();
          ++boardPos)
@@ -332,85 +390,82 @@ struct ConflictWithBoard
       const bool posConflict = (boardPos->second != testW);
       if (posOccupied && posConflict)
       {
-        return true;
+        conflict = true;
+        break;
       }
     }
-    return false;
+    // Undo mutation and return conflict.
+    UndoPly(ply, state);
+    return conflict;
   }
 
   BoardMap boardMap;
+  State* state;
 };
 
-/// <summary> Set of stable states with one weight. </summary>
+/// <summary> Set of stable plys with one weight. </summary>
 /// <remarks>
 ///   <para> The player to go last loses if any state with one weight is
 ///     reached since he must remove the final weight and tip the board.
 ///   </para>
-///   <para> To win with one weight there cannt be a pivot at 0. </para>
-///   </para>
+///   <para> To win with one weight there cannot be a pivot at 0. </para>
 /// </remarks>
-/// </summray>
-void OneWeightFinalSet(std::vector<State>* set)
+/// </summary>
+void OneWeightFinalSet(State* state, std::vector<Ply>* set)
 {
   assert(set);
+  assert(state);
   set->clear();
 
   // This is really just the plys possible from the board being empty.
-  State state;
-  InitState(&state);
-  const Board initBoard = state.board;
-  ClearBoard(&state.board);
-  state.turn = State::Turn_Blue;
-  std::vector<Ply> plys;
-  PossiblePlys(state, &plys);
-  set->resize(plys.size());
-  std::transform(plys.begin(), plys.end(),
-                 set->begin(),
-                 std::ptr_fun<const Ply&, State>(DoPly));
+  InitState(state);
+  const Board initBoard = state->board;
+  ClearBoard(&state->board);
+  state->turn = State::Turn_Blue;
+  PossiblePlys(*state, set);
   // Remove all conflicts to initial board.
   set->erase(std::remove_if(set->begin(), set->end(),
-                            ConflictWithBoard(initBoard)),
+                            ConflictWithBoard(initBoard, state)),
              set->end());
 }
 
-/// <summary> Set of states stable with two weights such that removal of either
+/// <summary> Set of plys stable with two weights such that removal of either
 ///   weight leads to a loss.
 /// </summary>
-void TwoWeightFinalSet(std::vector<State>* set)
+void TwoWeightFinalSet(State* state, std::vector<Ply>* set)
 {
   assert(set);
+  assert(state);
   set->clear();
 
   // This is the set of states reachable from the suicidal one states.
-  State state;
-  InitState(&state);
-  const Board initBoard = state.board;
-  ClearBoard(&state.board);
-  state.turn = State::Turn_Blue;
-  std::vector<Ply> plys;
-  SuicidalPlys(state, &plys);
-  std::vector<State> suicidalOneSet;
-  suicidalOneSet.resize(plys.size());
-  std::transform(plys.begin(), plys.end(),
-                 suicidalOneSet.begin(),
-                 std::ptr_fun<const Ply&, State>(DoPly));
+  InitState(state);
+  const Board initBoard = state->board;
+  ClearBoard(&state->board);
+  state->turn = State::Turn_Blue;
+  std::vector<Ply> suicidalPlys;
+  SuicidalPlys(*state, &suicidalPlys);
   // Erase all conflicts to initial board.
-  suicidalOneSet.erase(std::remove_if(suicidalOneSet.begin(),
-                                      suicidalOneSet.end(),
-                                      ConflictWithBoard(initBoard)),
-                       suicidalOneSet.end());
+  suicidalPlys.erase(std::remove_if(suicidalPlys.begin(), suicidalPlys.end(),
+                                    ConflictWithBoard(initBoard, state)),
+                     suicidalPlys.end());
   // Now get all reachable states from the suicidal set. Since each suicidal
   // state is unique, then the set of reachable states for each suicidal state
   // is also unique.
-  for (std::vector<State>::const_iterator suicideState = suicidalOneSet.begin();
-       suicideState != suicidalOneSet.end();
-       ++suicideState)
+  std::vector<Ply> plys;
+  for (std::vector<Ply>::const_iterator suicidePly = suicidalPlys.begin();
+       suicidePly != suicidalPlys.end();
+       ++suicidePly)
   {
     plys.clear();
-    PossiblePlys(*suicideState, &plys);
-    std::transform(plys.begin(), plys.end(),
-                   std::back_inserter(*set),
-                   std::ptr_fun<const Ply&, State>(DoPly));
+    // Mutate state to suicidal state.
+    DoPly(*suicidePly, state);
+    // Get possible plys from suicidal state.
+    PossiblePlys(*state, &plys);
+    set->reserve(set->size() + plys.size());
+    std::copy(plys.begin(), plys.end(), std::back_inserter(*set));
+    // Undo mutated stated.
+    UndoPly(*suicidePly, state);
   }
 }
 
@@ -437,15 +492,17 @@ int main(int /*argc*/, char** /*argv*/)
   }
   // Count states where blue wins.
   {
-    std::vector<State> oneW;
-    OneWeightFinalSet(&oneW);
+    State stateOneW;
+    std::vector<Ply> oneW;
+    OneWeightFinalSet(&stateOneW, &oneW);
     std::cout << "Blue wins from " << oneW.size() << " possible states "
               << "since Red cannot remove a weight." << std::endl;
   }
   // Count states where red wins.
   {
-    std::vector<State> twoW;
-    TwoWeightFinalSet(&twoW);
+    State stateTwoW;
+    std::vector<Ply> twoW;
+    TwoWeightFinalSet(&stateTwoW, &twoW);
     std::cout << "Red wins from " << twoW.size() << " possible states "
               << "since Blue cannot remove a weight." << std::endl;
   }
