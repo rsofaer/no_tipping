97e9bf4c6b9a13c296da2248a7035298dacd3043
Raphael Sofaer <raphael@joindiaspora.com>
2011-10-04 16:13:40 -0400

    Add a data string to the libntgni gtest file

--- libntgjni_gtest.h
+++ libntgjni_gtest.h
@@ -8,42 +8,62 @@
  */
 #include <string>
 #include <iostream>
-#include <stringstream>
+#include <sstream>
 
 namespace _no_tipping_game_jni_gtest_h_
 {
   const std::string startCommand(
-                                 "ADDING
-                                 0 0 Red 1
-                                 0 0 Blue 1
-                                 0 0 Red 2
-                                 0 0 Blue 2
-                                 0 0 Red 3
-                                 0 0 Blue 3
-                                 0 0 Red 4
-                                 0 0 Blue 4
-                                 0 0 Red 5
-                                 0 0 Blue 5
-                                 0 0 Red 6
-                                 0 0 Blue 6
-                                 0 0 Red 7
-                                 0 0 Blue 7
-                                 0 0 Red 8
-                                 0 0 Blue 8
-                                 0 0 Red 9
-                                 0 0 Blue 9
-                                 0 0 Red 10
-                                 0 0 Blue 10
-                                 1 -4 Green 3");
+                                 "ADDING"
+                                 "0 0 Red 1"
+                                 "0 0 Blue 1"
+                                 "0 0 Red 2"
+                                 "0 0 Blue 2"
+                                 "0 0 Red 3"
+                                 "0 0 Blue 3"
+                                 "0 0 Red 4"
+                                 "0 0 Blue 4"
+                                 "0 0 Red 5"
+                                 "0 0 Blue 5"
+                                 "0 0 Red 6"
+                                 "0 0 Blue 6"
+                                 "0 0 Red 7"
+                                 "0 0 Blue 7"
+                                 "0 0 Red 8"
+                                 "0 0 Blue 8"
+                                 "0 0 Red 9"
+                                 "0 0 Blue 9"
+                                 "0 0 Red 10"
+                                 "0 0 Blue 10"
+                                 "1 -4 Green 3");
   
-  const std::string inProgressCommand(
-                                      
+  const std::string invalidCommand(
+                                "ADDING"
+                                 "0 0 Red 1"
+                                 "0 0 Blue 1"
+                                 "0 0 Red 2"
+                                 "0 0 Blue 2"
+                                 "0 0 Red 3"
+                                 "1 -2 Blue 3"
+                                 "0 0 Red 4"
+                                 "0 0 Blue 4"
+                                 "0 0 Red 5"
+                                 "0 0 Blue 5"
+                                 "0 0 Red 6"
+                                 "0 0 Blue 6"
+                                 "0 0 Red 7"
+                                 "0 0 Blue 7"
+                                 "0 0 Red 8"
+                                 "0 0 Blue 8"
+                                 "0 0 Red 9"
+                                 "0 0 Blue 9"
+                                 "0 0 Red 10"
+                                 "0 0 Blue 10"
+                                 "1 -4 Green 3"); 
     
   
   using namespace hps;
   TEST(StateFromCommand, fills_out_state)
   {
-    
-    
+    ASSERT_FALSE(true);
   }
 }
